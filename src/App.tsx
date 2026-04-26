93c6a3e6dd2d332e33aad4f9b1286facabeb7efe
janpokenoob-cpu <jan.pokenoob@gmail.com>
2026-04-26 11:27:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import ClanOverview from './components/ClanOverview';
+import ClanOverview from './components/Clanoverview';
 import Esports from './components/Esports';
 import DiscordCTA from './components/DiscordCTA';
 import Footer from './components/Footer';
@@ -10,7 +9,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="bg-black min-h-screen text-white selection:bg-yellow-500/30 selection:text-yellow-500">
-      <Navbar />
       <main>
         <Hero />
         <About />
